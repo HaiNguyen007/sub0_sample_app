4bb0b906ffee6c7fba4993816f54eef56e33450b
Ruslan Talpa <ruslan.talpa@gmail.com>
2016-07-01 15:55:00 +0300

    by default do not log queries to stdout

--- sql/config.sh
+++ sql/config.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo "Enabling query logging"
-perl -pi -e "s/#log_statement = 'none'/log_statement = 'all'/g" /var/lib/postgresql/data/postgresql.conf
+
+# uncomment if you want to see the queries being executed by Sub0
+# echo "Enabling query logging"
+# perl -pi -e "s/#log_statement = 'none'/log_statement = 'all'/g" /var/lib/postgresql/data/postgresql.conf
